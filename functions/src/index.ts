09dc8cb1725506a79800556e7fe060d76de6b363
Daniel Hanover <hanover.daniel@hotmail.com>
2019-04-03 20:29:36 -0400

    Removed commented code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,32 +2,17 @@ import * as functions from 'firebase-functions';
 import * as moment from 'moment'
 
 import * as firebaseAdmin from 'firebase-admin'
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 export const HandleNotification = functions.firestore.document(`/pulse/{pulseId}`).onCreate(async (snap, context) => {
-    // const new_pulse = snap.data()
-
     try {
         const last_two_hours = (await snap.ref.parent
             .where(`timestamp`, `>`, moment().subtract(2, 'hour'))
             .orderBy(`timestamp`, 'asc')
             .get()).docs
     
-        // const firstPulseDate = moment(last_two_hours[0].data().timestamp.toDate())
         let wearing = 0; // seconds worn
-        // let not_wearing = 0; // seconds not worn
         for (let i = 0; i < last_two_hours.length - 1; i++) {
             if (last_two_hours[i].data().wearing) {
                 wearing += (last_two_hours[i + 1].data().timestamp.seconds - last_two_hours[i].data().timestamp.seconds)
-            }
-            else {
-                // not_wearing += (last_two_hours[i + 1].data().timestamp.seconds - last_two_hours[i].data().timestamp.seconds)
             }
         }
     
@@ -43,6 +28,6 @@ export const HandleNotification = functions.firestore.document(`/pulse/{pulseId}
             })
         }
     } catch (err) {
-        
+
     }
 })
